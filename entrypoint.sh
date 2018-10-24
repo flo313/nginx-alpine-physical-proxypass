b9fafbb0a2e178f7f20e055d4c75cd861900c5a3
flo313 <31136223+flo313@users.noreply.github.com>
2018-10-24 18:39:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,13 @@
 function ts {
   echo "[`date '+%Y-%m-%d %T'`] `basename "$(test -L "$0" && readlink "$0" || echo "$0")"`:"
 }
+echo "$(ts) Starting nginx server..."
+nginx -g "daemon off;"
 if [ -e ${NGINX_CONF} ] ; then
-	echo "$(ts) Starting nginx server..."
-	nginx -c ${NGINX_CONF}
-
 	echo "$(ts) Starting inotifywait loocking for changes on ${NGINX_CONF}..."
 	while inotifywait -q -r -e close_write,moved_to,create ${NGINX_CONF}; do
 	  echo "$(ts) Configuration changes detected. Will send reload signal to nginx in ${TIME_TO_UPDATE} seconds..."
 	  sleep ${TIME_TO_UPDATE}
 	  nginx -s reload && echo "$(ts) Reload signal send" && echo "$(ts) Loocking for another change on ${NGINX_CONF}"
 	done
-else
-	echo "$(ts) Unable to find ${NGINX_CONF}, starting nginx waiting its closure..."
-	nginx -g "daemon off;"
 fi
