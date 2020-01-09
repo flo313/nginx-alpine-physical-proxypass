3762a9ad166164bd4b8d9c05b9aca98231b1e810
flo313 <31136223+flo313@users.noreply.github.com>
2020-01-09 15:57:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
-#! /bin/sh
-
 function ts {
   echo "[`date '+%Y-%m-%d %T'`] `basename "$(test -L "$0" && readlink "$0" || echo "$0")"`:"
 }
@@ -7,13 +5,14 @@ echo "$(ts) Starting nginx server..."
 NGINX_CONF="${NGINX_CONF_PATH}/${NGINX_CONF_FILE}"
 NGINX_CONF_ENVSUBST="${NGINX_CONF_PATH}/_${NGINX_CONF_FILE}"
 if [ -e ${NGINX_CONF} ] ; then
-	envsubst < ${NGINX_CONF} > ${NGINX_CONF_ENVSUBST} && nginx -c ${NGINX_CONF_ENVSUBST}
-	echo "$(ts) Starting inotifywait loocking for changes on ${NGINX_CONF}..."
-	while inotifywait -q -r -e close_write,moved_to,create ${NGINX_CONF}; do
-	  echo "$(ts) Configuration changes detected. Will send reload signal to nginx in ${TIME_TO_UPDATE} seconds..."
-	  sleep ${TIME_TO_UPDATE}
-	  envsubst < ${NGINX_CONF} > ${NGINX_CONF_ENVSUBST} && nginx -s reload && echo "$(ts) Reload signal send" && echo "$(ts) Loocking for another change on ${NGINX_CONF}"
-	done
+    envsubst \$NGINX_CONF_PATH < ${NGINX_CONF} > ${NGINX_CONF_ENVSUBST} && nginx -c ${NGINX_CONF_ENVSUBST}
+    echo "$(ts) Starting inotifywait loocking for changes on ${NGINX_CONF_PATH} directory..."
+    while inotifywait -q -r -e close_write,moved_to,create ${NGINX_CONF_PATH}; do
+      echo "$(ts) Configuration changes detected. Will send reload signal to nginx in ${TIME_TO_UPDATE} seconds..."
+      sleep ${TIME_TO_UPDATE}
+      envsubst \$NGINX_CONF_PATH < ${NGINX_CONF} > ${NGINX_CONF_ENVSUBST} && nginx -s reload && echo "$(ts) Reload signal send" && echo "$(ts) Loocking for another change on ${NGINX_CONF_PATH}"
+      # nginx -s reload && echo "$(ts) Reload signal send" && echo "$(ts) Loocking for another change on ${NGINX_CONF_PATH}"
+    done
 else
-	nginx -g "daemon off;"
+    nginx -g "daemon off;"
 fi
